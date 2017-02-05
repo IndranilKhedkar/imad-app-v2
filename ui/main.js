1dd0cdb771cda77e3e7fba7798da94ce5b225d3b
IndranilKhedkar <indranilkhedkar@gmail.com>
2017-02-05 12:27:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var main_text= document.getElementById('main-text');
+main_text = "Hello! Welcome to IMAD";
+
+var margin=0;
+var img=document.getElementById('img');
+function moveRight(){
+    margin=margin+5;
+    img.style.marginLeft = marign + 'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
+
